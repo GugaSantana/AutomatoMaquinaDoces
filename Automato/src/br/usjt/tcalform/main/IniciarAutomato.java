71929f9d319811cc50ebe02b14e7469fb3dcd378
GugaSantana <gustavo_ssantana@hotmail.com>
2017-04-24 14:48:11 -0300

    Main na classe "IniciarAutomato"

---
+++ Automato/src/br/usjt/tcalform/main/IniciarAutomato.java
@@ -0,0 +1,10 @@
+package br.usjt.tcalform.main;
+
+import br.usjt.tcalform.view.AutomatoGUI;
+
+public class IniciarAutomato {
+	public static void main( String [] args)
+	{
+		new AutomatoGUI();
+	}
+}
